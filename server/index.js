722d10594b8b924d76b60cd60fb071854e82f756
Anguraj Ganesh <redphoenix0709@gmail.com>
2026-03-07 22:20:22 +0800

    oopsy
    oopsy

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import express from 'express'
 import cors from 'cors'
 import { getClient, createTable } from './clickhouse_auth.js'
 import apiRoutes from './routes/api.js'
+import epicentre from './routes/epicentre.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', apiRoutes)
+app.use('/epicentre', epicentre)
 
 // Connect to ClickHouse and ensure table exists
 const client = getClient()
---
+++ server/routes/epicentre.js
@@ -0,0 +1,70 @@
+import { Router } from 'express'
+const router = Router()
+
+
+
+
+
+
+
+import haversine from 'haversine'
+
+function getDistance(a, b) { // geojson positions are (lon, lat)
+    const point_a = {latitude: a[1], longitude: a[0]};
+    const point_b = {latitude: b[1], longitude: b[0]};
+    return haversine(point_a, point_b, {unit: 'meter'})
+}
+
+// EONET event categories:
+//  - drought
+//  - dustHaze
+//  - earthquakes
+//  - floods
+//  - landslides
+//  - manmade
+//  - seaLakeIce
+//  - severStorms
+//  - snow
+//  - tempExtremes
+//  - volcanoes
+//  - waterColor
+//  - wildfires
+
+
+
+async function getEvents() {
+    const url = "https://eonet.gsfc.nasa.gov/api/v3/events?limit=1";
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw Error('http error: ', response.status);
+        }
+        const data = await response.json();
+        
+        return data.events
+    } catch (error) {
+        console.error('unable to get events: ', error)
+    }
+}
+
+
+router.get('/', async (req, res) => {
+    const data = await getEvents();
+    const test_event = data[0];
+    const points = test_event.geometry;
+    console.log(test_event);
+    console.log(points);
+
+    for (let c = 0; c < points.length; c++) {
+        if (points[c].type == 'Point') {
+            console.log(points[c].coordinates);
+        }
+    }
+    
+    
+    console.log(getDistance(points[0].coordinates, points[1].coordinates));
+    
+});
+
+export default router
