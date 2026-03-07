6a1d13682050ea273adf620a1870b786f54ae147
Anguraj Ganesh <redphoenix0709@gmail.com>
2026-03-07 22:06:56 +0800

    fixed
    fixed

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
+import { getClient, createTable } from './clickhouse_auth.js'
 import apiRoutes from './routes/api.js'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use('/api', apiRoutes)
 
-app.listen(8009, () => console.log('express running on > 8009'))
+// Connect to ClickHouse and ensure table exists
+const client = getClient()
+await createTable(client)
+
+app.listen(8009, () => console.log('Server running on port 8009'))
