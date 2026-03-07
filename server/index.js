48e8e8b0c7928f44ac5cca1bbacdd97fc894185e
Anguraj Ganesh <redphoenix0709@gmail.com>
2026-03-07 21:30:35 +0800

    Revert "Ganesh"
    This reverts commit c8b20d94e029ee5e9d40de2164150772a1bfafad.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,8 @@
-import 'dotenv/config'
 import express from 'express'
-import cors from 'cors'
-import { getClient, createTable } from './clickhouse_auth.js'
 import apiRoutes from './routes/api.js'
 
 const app = express()
-app.use(cors())
 app.use(express.json())
 app.use('/api', apiRoutes)
 
-const client = getClient()
-await createTable(client)
-
-app.listen(8009, () => console.log('Server running on port 8009'))
+app.listen(8009, () => console.log('express running on > 8009'))
